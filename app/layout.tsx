170fa6f413b2b5996f616d11a9cbf67b84e10969
exosky12 <exoskyv2@gmail.com>
2024-02-19 15:24:11 +0100

    Analytics & SpeedInsights & next@canary

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { frFR } from "@clerk/localizations";
 import { dark } from "@clerk/themes";
 import { Header } from "./components/header/Header";
-import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const satoshi = localFont({
 	src: "./Satoshi-Variable.woff2",
--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,10 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/"],
-  ignoredRoutes: ["/((?!api|trpc))(_next.*|.+\.[\w]+$)", "/sign-in"]
+	publicRoutes: ["/"],
+	ignoredRoutes: ["/((?!api|trpc))(_next.*|.+.[w]+$)", "/sign-in"],
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/"],
+	matcher: ["/((?!.*\\..*|_next).*)", "/"],
 };
